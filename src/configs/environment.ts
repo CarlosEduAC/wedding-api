11e15aacd9b3db42aa2dea0e31bd04ce1cac8834
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2025-08-15 23:04:38 -0300

    fix: update configs

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -3,11 +3,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const NODE_ENV = process.env.NODE_ENV || 'development'
-export const ENV = process.env.ENV || ''
 export const PORT = process.env.PORT || 3000
-export const BU = process.env.BU || 'cross'
-export const TRIBE = process.env.TRIBE || 'financial'
-export const TAGS = process.env.TAGS
-export const LOG_LEVEL = process.env.LOG_LEVEL
-export const APP_VERSION = process.env.APP_VERSION
 export const DATABASE_URL = process.env.DATABASE_URL
