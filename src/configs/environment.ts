f1e6be28d185ef313bcb5323c0e1104b4667e9e1
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2025-08-15 22:47:50 -0300

    fix: updated dockerfile

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -10,3 +10,4 @@ export const TRIBE = process.env.TRIBE || 'financial'
 export const TAGS = process.env.TAGS
 export const LOG_LEVEL = process.env.LOG_LEVEL
 export const APP_VERSION = process.env.APP_VERSION
+export const DATABASE_URL = process.env.DATABASE_URL
