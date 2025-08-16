06aa66d42eeb1c3b11c9e97a2c71e184e29fa393
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2025-08-16 00:14:42 -0300

    feat: added invited create

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import HealthRouter from '@/routes/health.routes'
 import ProductRouter from '@/routes/product.routes'
+import InvitedRouter from '@/routes/invited.routes'
 
 const routes = Router()
 
 routes.use('/health', HealthRouter)
 routes.use('/products', ProductRouter)
+routes.use('/inviteds', InvitedRouter)
 
 export default routes
