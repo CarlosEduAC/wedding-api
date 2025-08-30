20dbf9bb95a26982daf18b46e0886e2220a126ca
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2025-08-30 12:51:05 -0300

    feat: added invited name routes

--- src/routes/invited.routes.ts
+++ src/routes/invited.routes.ts
@@ -5,6 +5,7 @@ import * as Controller from '@/controllers/invited.controller'
 const routes = Router()
 
 routes.get('/', Controller.findAll)
+routes.get('/names', Controller.findAllNames)
 routes.post('/', Controller.create)
 routes.put('/:id', Controller.update)
 routes.post('/many', Controller.createMany)
